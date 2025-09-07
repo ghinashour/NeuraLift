26517abaae931c7908927a51eb7375c8f267985c
Mhmdzein1 <mohammad_zein_2000@hotmail.com>
2025-09-08 00:14:34 +0300

    Features Page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import SuccessStories from './sections/SuccessStories';
-import Contact from './sections/Contact'; // import Contact section
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Features from "./sections/features";
+import SuccessStories from "./sections/SuccessStories";
+import Contact from "./sections/Contact"; // import Contact section
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      {/* Features Section */}
+      <Features />
       {/* Other components */}
       <SuccessStories />
 
