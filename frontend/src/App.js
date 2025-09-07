e73e66782f81eb5e1b5632fc55236fd106a4a0e6
omarmousa <omarmousa96103@gmail.com>
2025-09-07 19:49:26 +0300

    Added Home.js section to Navbar and Styled it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SuccessStories from './sections/SuccessStories';
 import Contact from './sections/Contact'; // import Contact section
+import Home from './sections/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Home />
       {/* Other components */}
       <SuccessStories />
 
